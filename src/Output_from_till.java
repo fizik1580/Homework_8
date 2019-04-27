b7457b8086c61c2e82a2aeaa1a0ee3520baa2791
fizik1580 <48645575+fizik1580@users.noreply.github.com>
2019-04-27 10:58:20 +0300

    Добавил константы для нижней/аерхней границы. Так правильнее мне кажется.

--- src/Output_from_till.java
+++ src/Output_from_till.java
@@ -2,14 +2,17 @@ public class Output_from_till {
 
     public static void main (String [] arg){
 
+        int upper_border = 50;
+        int lower_border = 1;
+
         System.out.println("Программа с помощью цикла выводит на экран числа от 1 до 50");
         System.out.print("Let's go \n");
 
-        for (int i = 1; i <= 50; i++){
+        for (int i = lower_border; i <= upper_border; i++){
 
             System.out.println(i);
             try {
-                Thread.sleep(100);
+                Thread.sleep(50);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
